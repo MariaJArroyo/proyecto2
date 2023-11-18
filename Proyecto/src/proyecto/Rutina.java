3f88222e82c394a335f633b55d3e5f753c1391df
MariaJArroyo <mjarroyocampos@gmail.com>
2023-11-18 15:58:47 -0600

    se les encarga  un espacio determinado para sus arreglos de objetos

--- Proyecto/src/proyecto/Rutina.java
+++ Proyecto/src/proyecto/Rutina.java
@@ -5,3 +5,34 @@ package proyecto;
 public class Rutina {
     
 }
+// Arreglos de objetos de Usuarios
+
+
+
+
+
+
+
+// Arreglos de objetos de Deportistas
+
+
+
+
+
+
+// Arreglos de objetos de Encargados
+
+
+
+
+
+
+// Arreglos de objetos de deportes
+
+
+
+
+
+
+
+// Arreglos de objetos de Rutinas
